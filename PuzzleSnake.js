ec4d84d41814a0f23ccf0de348ce24a6c7bbbbdc
slembcke <slembcke@gmail.com>
2013-08-25 03:54:05 -0500

    click handler

--- PuzzleSnake.js
+++ PuzzleSnake.js
@@ -1,5 +1,5 @@
 SVGNS = "http://www.w3.org/2000/svg";
-puzzle = {
+Puzzle = {
 	size: 5,
 	
 	tiles: [
@@ -7,8 +7,8 @@ puzzle = {
 		[0,1,0,0,0],
 		[0,0,0,1,0],
 		[0,0,0,0,0],
-		[0,0,0,0,0],
-	],
+		[0,0,0,0,0]
+	]
 };
 
 Board = document.getElementById("board");
@@ -19,6 +19,7 @@ function MakeProtoTypeNode(id){
 	return node;
 }
 
+TileProto = MakeProtoTypeNode("tile");
 TilesProto = MakeProtoTypeNode("tiles");
 PolylineProto = MakeProtoTypeNode("snake");
 HeadProto = MakeProtoTypeNode("head");
@@ -26,113 +27,129 @@ HeadProto = MakeProtoTypeNode("head");
 // TODO move this
 Tiles = Board.appendChild(TilesProto.cloneNode(true));
 
-{
-	// Setup scale matrix.
-	var svg = document.getElementById("svg");
-	var size = puzzle.size;
-	var w = svg.width.baseVal.value;
-	var h = svg.height.baseVal.value;
-	var sx = w/size;
-	var sy = h/size;
+function SetBoardSize(){
+	var size = Math.min(document.body.clientWidth, document.body.clientHeight);
 	
-	Board.setAttribute("transform", "matrix("+(sx)+", 0, 0, "+(-sy)+", "+(sx/2)+", "+(h - sy/2)+")");
+	var tiles = Puzzle.size;
+	var scale = size/tiles;
+	
+	var svg = document.getElementById("svg");
+	svg.setAttribute("width", size);
+	svg.setAttribute("height", size);
+	
+	Board.setAttribute("transform", "matrix("+(scale)+", 0, 0, "+(-scale)+", "+(scale/2)+", "+(size - scale/2)+")");
+	
+	ConvertCoords = function(x, y){
+		return [Math.floor(x/scale), tiles - 1 - Math.floor(y/scale)];
+	}
 }
+
+window.onresize = SetBoardSize;
+SetBoardSize();
 			
 function AddTile(x, y){
-	var bevel = 0.1;
-	var inset = 0.1;
-	
-	var s = 1.0 - 2.0*inset;
-	var hs = 0.5*s;
-	
-	var rect = document.createElementNS(SVGNS, "rect");
-	rect.x.baseVal.value = -hs;
-	rect.y.baseVal.value = -hs;
-	rect.width.baseVal.value = s;
-	rect.height.baseVal.value = s;
-	rect.rx.baseVal.value = bevel;
-	rect.ry.baseVal.value = bevel;
-	rect.setAttribute("fill", "darkcyan");
-	rect.setAttribute("transform", "matrix(1, 0, 0, 1, " + x + ", "+ y + ")");
-	Tiles.appendChild(rect);
+	var tile = TileProto.cloneNode(true);
+	tile.setAttribute("transform", "matrix(1, 0, 0, 1, "+(x)+", "+(y)+")");
+	Tiles.appendChild(tile);
 }
 
-for(var y=0; y<puzzle.size; y++){
-	for(var x=0; x<puzzle.size; x++){
-		if(puzzle.tiles[y][x]) AddTile(x, y);
+for(var y=0; y<Puzzle.size; y++){
+	for(var x=0; x<Puzzle.size; x++){
+		if(Puzzle.tiles[y][x]) AddTile(x, y);
 	}
 }
 
-function Snake(x, y){
-	this.facing = [1, 0];
-	this.verts = [[x, y], [x, y]];
-	this.polyline = PolylineProto.cloneNode(true);
-	this.head = HeadProto.cloneNode(true);
-}
-
-Snake.prototype.draw = function(){
-	this.polyline.setAttribute("points", this.verts.join(" "));
+Snake = (function(){
+	function This(startPos){
+		this.facing = [1, 0];
+		this.verts = [startPos, startPos.slice(0)];
+		this.polyline = PolylineProto.cloneNode(true);
+		this.head = HeadProto.cloneNode(true);
+	}
 	
-	var c = this.facing[0], s = this.facing[1];
-	var x = this.verts[0][0], y = this.verts[0][1];
-	this.head.setAttribute("transform", "matrix("+c+", "+s+", "+s+", "+c+", "+x+", "+y+")")
-}
-
-Snake.prototype.pushVert = function(v, animationCompleted){
-	console.log(v);
-	var x0 = this.verts[0][0], y0 = this.verts[0][1];
-	var x1 = v[0], y1 = v[1];
+	function headPos(){
+		return this.verts[0];
+	}
 	
-	var dx = x1 - x0, dy = y1 - y0;
-	var dist = Math.max(Math.abs(dx), Math.abs(dy));
-	this.facing = [dx/dist, dy/dist];
-	
-	var delay = 1.0/60.0;
-	var speed = 4.0;
-	var t = 0.0;
-	function animate(snake){
-		// TODO better time based animation here.
-		t = Math.min(1.0, t + speed*delay/dist);
+	This.prototype.draw = function(){
+		this.polyline.setAttribute("points", this.verts.join(" "));
 		
-		snake.verts[0] = [x0 + t*dx, y0 + t*dy];
-		snake.draw();
-		if(t < 1.0){
-			window.setTimeout(animate, delay, snake);
-		} else {
-			snake.verts.unshift(v);
-			if(animationCompleted) animationCompleted();
+		var c = this.facing[0], s = this.facing[1];
+		var x = this.verts[0][0], y = this.verts[0][1];
+		this.head.setAttribute("transform", "matrix("+c+", "+s+", "+s+", "+c+", "+x+", "+y+")")
+	}
+	
+	This.prototype.pushVert = function(v, animationCompleted){
+		console.log(v);
+		var x0 = this.verts[0][0], y0 = this.verts[0][1];
+		var x1 = v[0], y1 = v[1];
+		
+		var dx = x1 - x0, dy = y1 - y0;
+		var dist = Math.max(Math.abs(dx), Math.abs(dy));
+		this.facing = [dx/dist, dy/dist];
+		
+		var delay = 1.0/60.0;
+		var speed = 4.0;
+		var t = 0.0;
+		(function animate(snake){
+			// TODO better time based animation here.
+			t = Math.min(1.0, t + speed*delay/dist);
+			
+			snake.verts[0] = [x0 + t*dx, y0 + t*dy];
+			snake.draw();
+			if(t < 1.0){
+				window.setTimeout(animate, delay, snake);
+			} else {
+				snake.verts.unshift(v);
+				if(animationCompleted) animationCompleted();
+			}
+		})(this);
+	}
+	
+	This.prototype.addToBoard = function(){
+		Board.appendChild(this.polyline);
+		Board.appendChild(this.head);
+		this.draw();
+	}
+	
+	return This;
+})();
+
+Board.onclick = function(event){
+	var pos = ConvertCoords(event.clientX, event.clientY);
+	if(ClickHandler) ClickHandler(pos);
+};
+
+ClickHandler = function(pos){
+	var x = pos[0], y = pos[1];
+	
+	if(!Puzzle.tiles[y][x]){
+		Snake = new Snake(pos);
+		Snake.addToBoard();
+		
+		ClickHandler = function(pos){
+			console.log(pos);
 		}
 	}
-	
-	animate(this);
 }
 
-Snake.prototype.addToBoard = function(){
-	Board.appendChild(this.polyline);
-	Board.appendChild(this.head);
-	this.draw();
-}
-
-var snake = new Snake(3, 3);
-snake.addToBoard();
-
-(function Rec(verts){
-	var l = verts.length;
-	if(!l) return;
-	
-	snake.pushVert(verts[l - 1], function(){
-		Rec(verts.slice(0, l - 1));
-	});
-})([
-	[0, 1],
-	[0, 2],
-	[2, 2],
-	[2, 1],
-	[3, 1],
-	[3, 0],
-	[4, 0],
-	[4, 4],
-	[0, 4],
-	[0, 3],
-]);
+//(function Rec(verts){
+//	var l = verts.length;
+//	if(!l) return;
+//	
+//	snake.pushVert(verts[l - 1], function(){
+//		Rec(verts.slice(0, l - 1));
+//	});
+//})([
+//	[0, 1],
+//	[0, 2],
+//	[2, 2],
+//	[2, 1],
+//	[3, 1],
+//	[3, 0],
+//	[4, 0],
+//	[4, 4],
+//	[0, 4],
+//	[0, 3]
+//]);
 
